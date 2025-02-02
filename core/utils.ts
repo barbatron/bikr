37c178c53a5022c0daefb83a88b5329cc283d37f
Joakim Olsson <barbatron@gmail.com>
2025-02-02 18:45:45 +0100

    feat: log movememnt aggregation details, suppress zero-length buffers

--- core/utils.ts
+++ core/utils.ts
@@ -19,17 +19,23 @@ export const isLatLong = (x: unknown): x is LatLong =>
 //
 // RxJS utils
 //
-export const aggregateMovements = (movements: Movement[]): Movement => ({
-  // Relative movement is the sum of all movements in the buffer
-  relative: {
-    meters: movements.reduce(
-      (acc, curr) => acc + curr.relative.meters,
-      0,
-    ),
-  },
-  // Total movement from last entry in the buffer
-  total: movements[movements.length - 1]?.total ?? 0,
-});
+export const aggregateMovements = (movements: Movement[]): Movement => {
+  console.log("[utils:aggMovement] movements", {
+    count: movements.length,
+    distances: movements.map((m) => m.relative.meters),
+  });
+  return ({
+    // Relative movement is the sum of all movements in the buffer
+    relative: {
+      meters: movements.reduce(
+        (acc, curr) => acc + curr.relative.meters,
+        0,
+      ),
+    },
+    // Total movement from last entry in the buffer
+    total: movements[movements.length - 1]?.total ?? 0,
+  });
+};
 
 export const movementAggregator = (): OperatorFunction<Movement[], Movement> =>
   map(aggregateMovements);
@@ -40,6 +46,7 @@ export const bufferMovements =
       // Buffer & sum up movements to prevent spamming map api
       // Should ideally be synchronized with the street view panorama updates?
       bufferTime(duration),
+      filter((movements) => movements.length > 0),
       movementAggregator(),
       filter((m) => m.relative.meters > 0),
     );
